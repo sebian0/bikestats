9d04cff34005a0b9fee30d71f927da207cb206b3
spencerghoul <97404895+spencerghoul@users.noreply.github.com>
2022-01-23 14:15:26 -0800

    Create bikestats.py

---
+++ bikestats.py
@@ -0,0 +1,30 @@
+#imports
+from cgitb import text
+from fileinput import filename
+import os
+import openpyxl
+import datetime
+# import froms
+from openpyxl import Workbook
+#load
+Workbook_obj = openpyxl.load_workbook(r"C:\Users\spenc\Documents\bikestats\bikestats.xlsx")
+sheet_obj = Workbook_obj.active
+#Date/Time data
+DATE = input("Date")
+col1 = DATE
+
+#Miles data
+MILES = input("Miles")
+col2 = MILES
+
+# Time length data
+TIME = input("How long did you ride?")
+col3 = TIME
+
+# Heart rate data
+HTRATE = input("Average Heartrate")
+col4 = HTRATE
+
+sheet_obj.append([col1, col2, col3, col4])
+Workbook_obj.save("C:/Users/spenc/Documents/bikestats/bikestats.xlsx")
+

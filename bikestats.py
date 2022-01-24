6ec8cc04c73e5263899e2179c1446eeee9563f05
spencerghoul <97404895+spencerghoul@users.noreply.github.com>
2022-01-24 09:40:31 -0800

    Update to formatting
    format changes for inputs to allow for spacing in the command line. Makes it a little cleaner!

--- bikestats.py
+++ bikestats.py
@@ -10,19 +10,19 @@ from openpyxl import Workbook
 Workbook_obj = openpyxl.load_workbook(r"C:\Users\spenc\Documents\bikestats\bikestats.xlsx")
 sheet_obj = Workbook_obj.active
 #Date/Time data
-DATE = input("Date")
+DATE = input("Date: ")
 col1 = DATE
 
 #Miles data
-MILES = input("Miles")
+MILES = input("Miles: ")
 col2 = MILES
 
 # Time length data
-TIME = input("How long did you ride?")
+TIME = input("How long did you ride? ")
 col3 = TIME
 
 # Heart rate data
-HTRATE = input("Average Heartrate")
+HTRATE = input("Average Heartrate ")
 col4 = HTRATE
 
 sheet_obj.append([col1, col2, col3, col4])
